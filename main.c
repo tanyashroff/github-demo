0656e5fbf1f973c89bfb6e4c511cf466a6d19d0a
Tasneem Lightwala <tasneem.lightwala96@gmail.com>
2017-09-16 16:29:12 +0530

    Adding changed main.c

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "add.c"
+#include "sub.c"
+#include "power.c"
+#include "fact.c"
+
+int main()
+{
+		int a,b;
+		scanf("%d %d",&a,&b);
+		int res;
+		printf("Add %d Sub %d Power %d Fact %d",add(a,b),sub(a,b),power(a),fact(a));
+		return 0;
+}
